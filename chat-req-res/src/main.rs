cfb874160ec64db7d3bacb961a4a8d596d6d1a59
hirokuma <hiro9ma@gmail.com>
2025-11-17 18:22:11 +0900

    request_response

--- chat-req-res/src/main.rs
+++ chat-req-res/src/main.rs
@@ -1,20 +1,30 @@
-use std::{
-    collections::hash_map::DefaultHasher,
-    error::Error,
-    hash::{Hash, Hasher},
-    time::Duration,
-};
+use std::error::Error;
 
 use futures::stream::StreamExt;
 use libp2p::{
-    Swarm, Multiaddr, gossipsub, identity::Keypair, noise, swarm::{NetworkBehaviour, SwarmEvent}, tcp, yamux
+    Multiaddr, PeerId, StreamProtocol, noise,
+    request_response::{self, ProtocolSupport},
+    swarm::{NetworkBehaviour, SwarmEvent},
+    tcp, yamux,
 };
+use serde::{Deserialize, Serialize};
 use tokio::{io, io::AsyncBufReadExt, select};
 use tracing_subscriber::EnvFilter;
 
+// Request/Responseで送受信するメッセージ型
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct ChatRequest {
+    data: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct ChatResponse {
+    data: String,
+}
+
 #[derive(NetworkBehaviour)]
 struct MyBehaviour {
-    gossipsub: gossipsub::Behaviour,
+    request_response: request_response::cbor::Behaviour<ChatRequest, ChatResponse>,
 }
 
 #[tokio::main]
@@ -26,27 +36,29 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_env_filter(EnvFilter::from_default_env())
         .try_init();
 
-    let use_quic = if let Some(arg) = std::env::args().nth(1) {
-        arg == "quic"
-    } else {
-        false
-    };
-    println!("use: quic={}", use_quic);
-    let fn_swarm = get_swarm_fn(use_quic);
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,     // noise, tls, plaintext(for test), ...
+            yamux::Config::default, // yamux, mplex, ...
+        )?
+        .with_behaviour(|_| MyBehaviour {
+            request_response: request_response::cbor::Behaviour::<ChatRequest, ChatResponse>::new(
+                [(StreamProtocol::new("/chat-chat/1"), ProtocolSupport::Full)],
+                request_response::Config::default(),
+            ),
+        })?
+        .build();
 
-    // QUICの有無をオプションで変更できるようにしたかったが .with_quic()の有無で型が変わるので止めた
-    let mut swarm = fn_swarm.0()?;
-
-    // Create a Gossipsub topic
-    let topic = gossipsub::IdentTopic::new("test-net");
-    // subscribes to our topic
-    swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
+    let peer_id = swarm.local_peer_id();
+    println!("My peer ID: {}", peer_id);
 
     // Read full lines from stdin
     let mut stdin = io::BufReader::new(io::stdin()).lines();
 
     // Listen on all interfaces and whatever port the OS assigns
-    fn_swarm.1(&mut swarm)?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     // 引数があれば接続先として扱う
     if let Some(addr) = std::env::args().nth(1) {
@@ -57,147 +69,58 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("Not dialed");
     }
 
-    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
+    println!("Enter messages via STDIN and they will be sent to connected peers");
 
     // gossipsubの仕様でmessageIdが同じになるとpublish()でDuplicateエラーになる。
     // message_id_fn の実装でmessageIdの計算方法を変更できる。
+    let mut connected_peer_id = PeerId::random();
     loop {
         select! {
             Ok(Some(line)) = stdin.next_line() => {
-                if let Err(e) = swarm
-                    .behaviour_mut().gossipsub
-                    .publish(topic.clone(), line.as_bytes()) {
-                        println!("Publish error: {e:?}");
-                    }
-            }
+                // https://deepwiki.com/search/requestresponse-behaviour_3732d1e4-4ee6-49e1-8805-e10941b9b463?mode=fast
+                println!("input: {line}");
+                let id = swarm.behaviour_mut()
+                    .request_response
+                    .send_request(&connected_peer_id, ChatRequest{data: line});
+                println!("send request id: {}", id);
+            },
             event = swarm.select_next_some() => match event {
                 // 通信系イベント?
 
-                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
-                    propagation_source: peer_id,
-                    message_id: id,
-                    message,
-                })) => {
-                    let msg = String::from_utf8_lossy(&message.data);
-                    println!(
-                        "Got message: '{msg}' with id: {id} from peer: {peer_id}",
-                    );
-                    if msg == "HELLO" {
-                        if let Err(e) = swarm
-                            .behaviour_mut()
-                            .gossipsub
-                            .publish(topic.clone(), b"WORLD") {
-                            println!("Publish error after got message: {e:?}");
-                        }
-                    } else if msg == "WORLD" {
-                        if let Err(e) = swarm
-                            .behaviour_mut()
-                            .gossipsub
-                            .publish(topic.clone(), b"HELLO") {
-                            println!("Publish error after got message: {e:?}");
-                        }
-                    }
-                },
                 SwarmEvent::NewListenAddr { address, .. } => {
                     println!("Local node is listening on {address}");
-                }
+                },
+                SwarmEvent::ConnectionEstablished {peer_id, connection_id: _, endpoint: _, num_established: _, concurrent_dial_errors: _, established_in: _ } => {
+                    connected_peer_id = peer_id;
+                    println!("connected: {}", connected_peer_id);
+                },
+                SwarmEvent::ConnectionClosed { peer_id: _, connection_id: _, endpoint: _, num_established: _, cause: _ } => println!("disconnected"),
+                // SwarmEvent::Behaviour(event) => println!("{event:?}"),
+                SwarmEvent::Behaviour(MyBehaviourEvent::RequestResponse(request_response::Event::Message {
+                    peer: _,
+                    connection_id: _,
+                    message: request_response::Message::Request { request_id: _, request, channel },
+                })) => {
+                    println!("request: {}", request.data);
+                    if let Err(e) = swarm
+                        .behaviour_mut()
+                        .request_response
+                        .send_response(channel, ChatResponse{data: "WORLD".to_string()}) {
+                        println!("response send error: {e:?}");
+                    } else {
+                        println!("send response");
+                    }
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::RequestResponse(request_response::Event::Message {
+                    peer: _,
+                    connection_id: _,
+                    message: request_response::Message::Response { request_id: _, response }
+                })) => {
+                    println!("response: {}", response.data);
+                },
+
                 _ => {}
             }
         }
     }
-}
-
-// QUICの有無を分けたかったら元から分けるのが一番楽。
-// ちなみに私はQUICプロトコルのことを知らない。
-//  https://ja.wikipedia.org/wiki/QUIC
-fn swarm_with_quic() -> Result<Swarm<MyBehaviour>, Box<dyn Error>> {
-    let swarm = libp2p::SwarmBuilder::with_new_identity()
-        .with_tokio()
-        .with_tcp(
-            tcp::Config::default(),
-            noise::Config::new, // noise, tls, plaintext(for test), ...
-            yamux::Config::default, // yamux, mplex, ...
-        )?
-        .with_quic()
-        .with_behaviour(my_behaviour)?
-        .build();
-    Ok(swarm)
-}
-
-fn listen_with_quic(swarm: &mut Swarm<MyBehaviour>) -> Result<(), Box<dyn Error>> {
-    swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-    Ok(())
-}
-
-fn swarm_without_quic() -> Result<Swarm<MyBehaviour>, Box<dyn Error>> {
-    let swarm = libp2p::SwarmBuilder::with_new_identity()
-        .with_tokio()
-        .with_tcp(
-            tcp::Config::default(),
-            noise::Config::new, // noise, tls, plaintext(for test), ...
-            yamux::Config::default, // yamux, mplex, ...
-        )?
-        .with_behaviour(my_behaviour)?
-        .build();
-    Ok(swarm)
-}
-
-fn listen_without_quic(swarm: &mut Swarm<MyBehaviour>) -> Result<(), Box<dyn Error>> {
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-    Ok(())
-}
-
-fn get_swarm_fn(use_quic: bool) ->
-    (
-        fn() -> Result<Swarm<MyBehaviour>, Box<dyn Error>>,
-        fn(&mut Swarm<MyBehaviour>) -> Result<(), Box<dyn Error>>,
-    )
-{
-    if use_quic {
-        (swarm_with_quic, listen_with_quic)
-    } else {
-        (swarm_without_quic, listen_without_quic)
-    }
-}
-
-fn my_behaviour(key: &Keypair) -> MyBehaviour {
-    behaviour(key).expect("build behaviour for MyBehaviour")
-}
-
-fn behaviour(key: &Keypair) -> Result<MyBehaviour, Box<dyn Error>> {
-    // ここでMessageIdを計算している。
-    // GossipSubは同じMessageIdのブロードキャストをエラーにするので暫定で時間要素を入れている
-    let message_id_fn = |message: &gossipsub::Message| {
-        let now = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_millis();
-        let mut v = now.to_le_bytes().to_vec();
-        v.extend_from_slice(&message.data);
-
-        let mut s = DefaultHasher::new();
-        v.hash(&mut s);
-        let h = s.finish().to_string();
-        println!("hash: {}", h);
-        gossipsub::MessageId::from(h)
-    };
-
-    // Set a custom gossipsub configuration
-    let gossipsub_config = gossipsub::ConfigBuilder::default()
-        .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-        .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
-        // signing)
-        .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
-        .build()
-        .map_err(io::Error::other)?; // Temporary hack because `build` does not return a proper `std::error::Error`.
-        //(Copilot提案) .map_err(|e| Box::<dyn Error>::from(e))?; // Map build error into boxed error.
-
-    // build a gossipsub network behaviour
-    let gossipsub = gossipsub::Behaviour::new(
-        gossipsub::MessageAuthenticity::Signed(key.clone()),
-        gossipsub_config,
-    )?;
-
-    Ok(MyBehaviour { gossipsub })
 }
